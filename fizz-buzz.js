dfc94ba622288164dc5e608e52cf86b97506132c
tumit <tumit@odd.works>
2017-11-06 01:35:59 +0700

    add fizz-buzz.js add require fizz-buzz module to spec

---
+++ fizz-buzz.js
@@ -0,0 +1,3 @@
+exports.say = () => {
+    return 1;
+}
--- fizz-buzz.spec.js
+++ fizz-buzz.spec.js
@@ -1,5 +1,16 @@
-describe('Fizz Buzz', () =>  {
+var fizzBuzz = require("./fizz-buzz");
+
+describe('Fizz Buzz', () => {
     it('should be OK', () => {
         expect(true).toBe(true);
     });
+
+    describe('when say', () => {
+        it('should be 1 when 1', () => {
+            expect(fizzBuzz.say(1)).toBe(1);
+        });
+    });
+
+
+
 });
